973c127641e7a10e1e956afaaaf20d410d115eb9
Temur5319436 <temur5319436@gmail.com>
2023-12-05 12:00:50 +0500

    feat: environs is installed and settings.py is changed

--- settings.py
+++ settings.py
@@ -1,36 +1,37 @@
 import os
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: Modify this secret key if using in production!
-SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
+SECRET_KEY = env.str("SECRET_KEY")
 
-DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": env.str("DB_HOST"),
+        "PORT": env.str("DB_PORT"),
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASSWORD"),
     }
 }
 
+INSTALLED_APPS = "db"
 
-"""
-To connect to an existing postgres database, first:
-pip install psycopg2
-then overwrite the settings above with:
+BOT_TOKEN = env.str("BOT_TOKEN")
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'YOURDB',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-"""
+WEBHOOK_HOST = env.str("WEBHOOK_HOST")
+WEBHOOK_PATH = env.str("WEBHOOK_PATH")
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-INSTALLED_APPS = ("db",)
+WEBAPP_HOST = env.str("WEBAPP_HOST")
+WEBAPP_PORT = env.str("WEBAPP_PORT")
+
+ADMINS = env.list("ADMINS", [])
